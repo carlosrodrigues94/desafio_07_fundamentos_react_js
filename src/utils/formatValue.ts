022df8beec9b5749e0283f67b5f6a9433dc79544
Carlos Henrique Rodrigues Santos <51969060+carlosrodrigues94@users.noreply.github.com>
2020-04-26 21:03:09 -0300

    first commit

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,8 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+const formatValue = (value: number): string => {
+  return Intl.NumberFormat([], {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value); // TODO
+};
 
 export default formatValue;
